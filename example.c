02dedf1bf669578406163c18dff4d29bb83d77c8
TheNogl <19914064+TheNogl@users.noreply.github.com>
2020-04-16 00:23:44 +0200

    Test program update

--- example.c
+++ example.c
@@ -1,12 +1,12 @@
-#include "RFM69.h"
+//#include "RFM69.h"
 #include <stdio.h>
 
-#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY     RF69_868MHZ
 
 int main(void)
 {
-    initialize(FREQUENCY, 1, 100, 16, 22, 0);
-    readTemperature(0);
+    //initialize(FREQUENCY, 1, 100, 16, 22, 0);
+    //readTemperature(0);
     char string[20];
     while(1)
     {
@@ -14,10 +14,10 @@ int main(void)
         scanf("%s", string);
         if(strstr(string, "Animation"))
         {
-            if(sendWithRetry(2,"animation1,15",13,3,10))
-                printf("Ack Received");
-            else
-                printf("No Ack Received");
+            //if(sendWithRetry(2,"animation1,15",13,3,10))
+            printf("Ack Received");
+            //else
+                //printf("No Ack Received");
         }
     }
     return 0;
